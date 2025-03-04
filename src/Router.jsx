3ca0150a5ade3071e4f3be375b978da10d5c843c
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-04 21:18:53 +0530

    layout setup done!

---
+++ src/Router.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+import { Children } from "react";
+const routes = [
+  {
+    path: "/",
+        element: <App />,
+        Children:[ {
+        
+        }
+    ]
+  },
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router';
+import router from './Router.jsx';
 
-createRoot(document.getElementById('root')).render(<App />
+createRoot(document.getElementById('root')).render(
   
+    < RouterProvider router={router} />
 );
