0d9142d6286c9b1c2f5e852e8b29eb0d288ff72c
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-05 01:04:10 +0530

    use state

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,22 +1,25 @@
-import {Link} from "react-router"
+import { Link } from "react-router"
+
 const Layout = ({ children }) => {
   return (
-      <div className='flex flex-col min-h-screen'>
-          <nav className="flex justify-between bg-gray-900 p-4 text-white" >
-              <div>
-                  <Link to="/" className='mr-4'>Home</Link>
-                  <Link to="/register" className='mr-4'>Register</Link>
-                  <Link to="/login" className='mr-4'>Login</Link>
-              </div>
-          </nav>
-          
-          <main className="flex-grew"> 
-              {children}
-              
-          </main>
-          
-      </div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <nav className="flex justify-between bg-gray-900 p-4 text-white">
+        <div>
+          <Link to="/" className="mr-4">
+            Home
+          </Link>
+          <Link to="/register" className="mr-4">
+            Register
+          </Link>
+          <Link to="/login" className="mr-4">
+            Login
+          </Link>
+        </div>
+      </nav>
+
+      <main className="flex-grew">{children}</main>
+    </div>
+  );
+};
 
 export default Layout;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
-
+import { useState } from 'react';
 const Register = () => {
+  const [form, setForm] = useState ({
+    name: "",
+    email: "",
+    password: ""
+  });
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    console.log("Registering.....");
+    console.log(form);
+  };
+
   return (
     <div className="mx-auto mt-20 p-4 border rounded max-w-xs">
       <h2 className="mb-4 text-xl">Candidate Registration</h2>
-      <from className="flex flex-col space-y-3">
+      <form className="flex flex-col space-y-3" onSubmit={handleRegister}>
         <input
           name="name"
           type="text"
           placeholder="Name"
           className="p-2 border rounded"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
         />
         <input
           name="email"
           type="email"
           placeholder="Email"
           className="p-2 border rounded"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
         />
         <input
           name="password"
           type="password"
           placeholder="Password"
           className="p-2 border rounded"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
         />
         <button className="bg-blue-500 py-2 rounded text-white" type="submit">
           Register
         </button>
-      </from>
+      </form>
     </div>
   );
-}
+};
 
 export default Register;
